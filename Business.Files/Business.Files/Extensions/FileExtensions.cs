ae1e1d5b3da61f6e309243e3d9f1a7d2d6add3c6
Joao Gouveia <jevg@live.com.pt>
2021-05-19 23:02:51 +0100

    Initial Commit

---
+++ Business.Files/Business.Files/Extensions/FileExtensions.cs
@@ -0,0 +1,96 @@
+﻿namespace Business.Files.Extensions
+{
+    using Common.ExceptionHandler.Exceptions;
+    using Data.Files;
+    using Microsoft.AspNetCore.Http;
+
+    public static class FileExtensions
+    {
+        /// <summary>
+        /// Update modified fields
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pEntity"></param>
+        /// <param name="pEntity2"></param>
+        /// <param name="pContext"></param>
+        public static void UpdateModifiedFields<T>(this T pEntity, T pEntity2, ref FilesDbContext pContext)
+        {
+            var entry = pContext.Entry(pEntity);
+            foreach (var prop in entry.Properties)
+            {
+                var value = pEntity2.GetType().GetProperty(prop.Metadata.Name).GetValue(pEntity2);
+                if (value != null)
+                {
+                    prop.CurrentValue = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Upload file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="path">Path to upload</param>
+        public static void UploadToDisk(this IFormFile file, string path)
+        {
+            try
+            {
+                using var stream = System.IO.File.Create(path);
+                file.CopyTo(stream);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Deletes file
+        /// </summary>
+        /// <param name="path">Path to file</param>
+        public static void DeleteFromDisk(string path)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(path))
+                    throw new InternalException("Path to delete is null or empty");
+
+                if (System.IO.File.Exists(path))
+                    System.IO.File.Delete(path);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Updates file
+        /// </summary>
+        /// <param name="path">Path to file</param>
+        public static void UpdateToDisk(this IFormFile file, string path)
+        {
+            {
+                try
+                {
+                    if (System.IO.File.Exists(path))
+                    {
+                        // If file found, delete it    
+                        DeleteFromDisk(path);
+
+                        //Upload new file
+                        file.UpdateToDisk(path);
+                    }
+                    //If file does not exist upload it
+                    else
+                        file.UploadToDisk(path);
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
+
---
+++ Business.Files/Business.Files/Services/FileService.cs
@@ -0,0 +1,166 @@
+﻿using Business.Files.Extensions;
+using Common.ExceptionHandler.Exceptions;
+using Data.Files;
+using Data.Files.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Files.Services
+{
+    public class FileService
+    {
+        /// <summary>
+        /// Files db context
+        /// </summary>
+        private FilesDbContext _context;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public FileService(FilesDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Save file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public async Task<int> Save(IFormFile file)
+        {
+            try
+            {
+                var fileInfo = new File()
+                {
+                    ContentType = file.ContentType,
+                    Extension = file.FileName.Split(".").LastOrDefault(),
+                    OriginalName = file.FileName,
+                    Path = ConfigurationManager.AppSettings["DefaultFilesUploadLocation"],
+                    CreateDate = DateTime.Now
+                };
+
+                fileInfo.Path += fileInfo.Key;
+
+                //Upload to disk
+                file.UploadToDisk(fileInfo.Path);
+
+                //Save file information to db
+                _context.Files.Add(fileInfo);
+
+                await _context.SaveChangesAsync();
+
+                return fileInfo.Id;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load file by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public File Load(int id)
+        {
+            try
+            {
+                return _context.Files.SingleOrDefault(x => x.Id == id);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load file by key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public File Load(string key)
+        {
+            try
+            {
+                return _context.Files.SingleOrDefault(x => x.Key == key);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Update file
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="file"></param>
+        /// <param name="formFile"></param>
+        /// <returns></returns>
+        public async Task<int> Update(int id, File file, IFormFile formFile)
+        {
+            try
+            {
+                var oldFile = Load(id);
+
+                if (oldFile == null)
+                    throw new NotFoundException("File not found");
+
+                //Update old message fields
+                oldFile.UpdateModifiedFields(file, ref _context);
+
+                oldFile.UpdateDate = DateTime.Now;
+
+                _context.Update(oldFile);
+
+                await _context.SaveChangesAsync();
+
+                if (formFile != null)
+                    formFile.UpdateToDisk(oldFile.Path);
+
+                return oldFile.Id;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Delete file
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<int> Delete(int id)
+        {
+            try
+            {
+                var file = Load(id);
+
+                if (file == null)
+                    throw new NotFoundException("File not found");
+
+                _context.Remove(file);
+
+                await _context.SaveChangesAsync();
+
+                //Remove file from disk
+                FileExtensions.DeleteFromDisk(file.Path);
+
+                return file.Id;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
